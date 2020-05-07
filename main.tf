6ac9fba6a7b25b35894471cffdcb60564c0c8e07
praveenkavedi <kvpraveenkavedi@outlook.com>
2020-05-07 20:33:20 +0530

    giving credentials seperately in var file

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
-//  access_key = "AKIA46S6PAL3IFGP2YLA"
-//  secret_key = "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"
+  access_key = "${var.AWS_ACCESS_KEY_ID}"
+  secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
 }
 
 resource "aws_vpc" "CICDvpc" {
